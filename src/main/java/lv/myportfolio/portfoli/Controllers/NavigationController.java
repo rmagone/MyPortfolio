89129d2a23a8257684c425b4b0862ba496c42aa4
Rudolfs Magone <rudolfs.magone@28stone.com>
2024-12-17 19:10:53 +0200

    -- minor tweaks for headers

--- src/main/java/lv/myportfolio/portfoli/Controllers/NavigationController.java
+++ src/main/java/lv/myportfolio/portfoli/Controllers/NavigationController.java
@@ -1,23 +1,27 @@
 package lv.myportfolio.portfoli.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NavigationController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("currentPage","home");
         return "index";
     }
 
     @GetMapping("/about_me")
-    public String aboutMe() {
+    public String aboutMe(Model model) {
+        model.addAttribute("currentPage","about");
         return "aboutMe";
     }
 
     @GetMapping("/portfolio")
-    public String portfolio() {
+    public String portfolio(Model model) {
+        model.addAttribute("currentPage","portfolio");
         return "portfolio";
     }
 
